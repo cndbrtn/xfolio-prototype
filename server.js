42131bda8daa8c33af26bdace24ebb722b417314
Cindy Burton <cindy.brton@gmail.com>
2020-04-17 14:56:29 -0600

    klsdfjlsdk

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
-app.use(routes); // all dem routes
 // this fixes a problem with blank pages for the :username/gallery and :username/blog but it also kills all my api routes
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
-    app.get('*', (req, res) => { res.sendfile(path.join(__dirname = '/client/build/index.html')); });
+    app.get('*', (req, res) => { res.sendfile(path.join(__dirname + '/client/build/index.html')); });
 }
 
+app.use(routes); // all dem routes
 
 // start api server
 app.listen(PORT, () => {
