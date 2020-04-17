7dba644cff40402da152cf8e6eccc32500367ef2
Cindy Burton <cindy.brton@gmail.com>
2020-04-17 13:58:55 -0600

    nothing will ever work

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,8 @@ router.use('/api', apiRoutes);
 router.use(awsRoutes)
 
 // If no API routes are hit, send the React app
-router.use(function (req, res) {
-    res.sendFile(path.join(__dirname, '../client/public/index.html'));
+router.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 module.exports = router;
