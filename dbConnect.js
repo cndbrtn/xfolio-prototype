2de45c31e22b51285dc032fd2a059f6023b154a8
Cindy Burton <cindy.brton@gmail.com>
2020-04-17 14:15:57 -0600

    this won't work either

--- dbConnect.js
+++ dbConnect.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise;
 
-const uri = process.env.MONGODB_URI || 'mongodb://localhost/artdb'
+const uri = process.env.MONGODB_URI || process.env.MONGO_DB
 
 mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true }).then(
     () => {
