fa55fdb5e194e23311f5041719f413136fc285c9
Cindy Burton <cindy.brton@gmail.com>
2020-04-17 13:49:19 -0600

    nothing will ever work and I'm tired

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.use(passport.session());
 // this fixes a problem with blank pages for the :username/gallery and :username/blog but it also kills all my api routes
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => { res.sendfile(path.join(__dirname = 'client/build/index.html')); });
+    app.get('/', (req, res) => { res.sendfile(path.join(__dirname = 'client/build/index.html')); });
 }
 
 app.use(routes); // all dem routes
